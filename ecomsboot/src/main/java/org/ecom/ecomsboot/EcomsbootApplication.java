48d3e33d9fec2eaaaf4f0e78de3eae5d44a643fc
Md. Zawad Hossain Rifat <99504215+Rifat-H7@users.noreply.github.com>
2023-12-25 14:16:23 +0600

    fbup

---
+++ ecomsboot/src/main/java/org/ecom/ecomsboot/EcomsbootApplication.java
@@ -0,0 +1,13 @@
+package org.ecom.ecomsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomsbootApplication.class, args);
+    }
+
+}
---
+++ ecomsboot/src/test/java/org/ecom/ecomsboot/EcomsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecom.ecomsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomsbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
